afec7f8f4ef5c53c17ae05bdd6439095cf679288
Wasif Fahim <wasif.fahim@gmail.com>
2021-01-26 17:12:39 -0800

    created the baseline for ig post

---
+++ whova-ig/src/components/PortraitPost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+
+class PortraitPost extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            comment: '',
+        }
+    }
+}
---
+++ whova-ig/src/redux/actions/types.js
@@ -0,0 +1,5 @@
+export const GET_ALL_COMMENTS = 'GET_ALL_COMMENTS';
+export const LIKE_COMMENT = 'LIKE_COMMENT';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const LIKE_REPLY = 'LIKE_REPLY';
+export const REPLY_TO_COMMENT = 'REPLY_TO_COMMENT';
